5d4f0ef1605d0550b252ea259861c35656c3da63
yangqi <sloanyang@gmail.com>
2014-08-21 17:09:07 +0800

    Update Server Ip address

--- tools/EventLogging/src/com/example/eventlogging/LogUploader.java
+++ tools/EventLogging/src/com/example/eventlogging/LogUploader.java
@@ -35,7 +35,7 @@ public class LogUploader {
 	
 	public static final int UPLOAD_THRESHOLD = 15*1024*1024;//15M 
 	
-	private static final String SERVER_IP = "gambit.eecs.umich.edu"; 
+	private static final String SERVER_IP = "10.33.43.61"; 
 	private static final int SERVER_PORT = 5204;
 	
 	private ConnectivityManager connectivityManager;
